abcec42cd59455db9875e449856fccfe99e629fa
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-03 16:25:51 +0900

    wip

--- src/libraries/html/get.lib.ts
+++ src/libraries/html/get.lib.ts
@@ -6,54 +6,53 @@ import { PrismaLibrary } from "libraries/common/prisma.lib";
 import fetch from "node-fetch";
 import { NewsArrayType } from "types/news.type";
 
-@Injectable()
-export class ScrapeManager {
-  constructor(private prisma: PrismaLibrary) {}
+export const scrapeHackerNews = async () => {
+  try {
+    const prisma = new PrismaLibrary();
 
-  async scrapeHackerNews() {
-    try {
-      const newsArray: NewsArrayType[] = [];
+    const newsArray: NewsArrayType[] = [];
 
-      const html = await axios.get("https://news.ycombinator.com/");
+    const html = await axios.get("https://news.ycombinator.com/");
 
-      const scrapedHtml = cheerio.load(html.data);
+    const scrapedHtml = cheerio.load(html.data);
 
-      const rank = scrapedHtml("table")
-        .children("tbody")
-        .children("tr.athing")
-        .children("td")
-        .children("span.rank")
-        .append("!")
-        .text()
-        .split("!");
+    // 랭크
+    const rank = scrapedHtml("table")
+      .children("tbody")
+      .children("tr.athing")
+      .children("td")
+      .children("span.rank")
+      .append("!")
+      .text()
+      .split("!");
 
-      const posts = scrapedHtml("table")
-        .children("tbody")
-        .children("tr.athing")
-        .children("td.title")
-        .children("span.titleline")
-        .children("a")
-        .append("!")
-        .text()
-        .split("!");
+    // 포스트 제목
+    const posts = scrapedHtml("table")
+      .children("tbody")
+      .children("tr.athing")
+      .children("td.title")
+      .children("span.titleline")
+      .children("a")
+      .append("!")
+      .text()
+      .split("!");
 
-      posts.filter((item, index) => {
-        newsArray.push({ rank: rank[index], post: item });
-      });
+    posts.filter((item, index) => {
+      newsArray.push({ rank: rank[index], post: item });
+    });
 
-      await this.prisma.hackers.createMany({
-        data: newsArray,
-      });
+    await prisma.hackers.createMany({
+      data: newsArray,
+    });
 
-      Logger.log("News Array: %o", { newsArray });
+    Logger.log("News Array: %o", { newsArray });
 
-      return newsArray;
-    } catch (error) {
-      throw new ScrapeError(
-        "HTML Scrape",
-        "Web Scraping Error",
-        error instanceof Error ? error : new Error(JSON.stringify(error))
-      );
-    }
+    return newsArray;
+  } catch (error) {
+    throw new ScrapeError(
+      "HTML Scrape",
+      "Web Scraping Error",
+      error instanceof Error ? error : new Error(JSON.stringify(error))
+    );
   }
-}
+};
