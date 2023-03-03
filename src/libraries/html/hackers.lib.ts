cb42bd22812920315f64227e2c76a62657610d2d
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-03 16:32:05 +0900

    wip

--- src/libraries/html/hackers.lib.ts
+++ src/libraries/html/hackers.lib.ts
@@ -5,13 +5,19 @@ import { ScrapeError } from "errors/scrape.error";
 import { PrismaLibrary } from "libraries/common/prisma.lib";
 import { NewsArrayType } from "types/news.type";
 
+/**
+ * Hackers News 1 ~ 30: "https://news.ycombinator.com/"
+ * @returns
+ */
 export const scrapeHackerNews = async () => {
   try {
+    const hackerUrl = "https://news.ycombinator.com/";
+
     const prisma = new PrismaLibrary();
 
     const newsArray: NewsArrayType[] = [];
 
-    const html = await axios.get("https://news.ycombinator.com/");
+    const html = await axios.get(hackerUrl);
 
     const scrapedHtml = cheerio.load(html.data);
 
