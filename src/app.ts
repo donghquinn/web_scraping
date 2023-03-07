effff382f233f93549258e0ef93d591d5def12ea
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-07 12:50:03 +0900

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import "./env";
+
 import { ScrapeObserver } from "libraries/manager.lib";
 import { bootstrap } from "server";
 
--- src/modules/prisma.module.ts
+++ src/modules/prisma.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { PrismaLibrary } from "libraries/common/prisma.lib";
 
 @Module({
-  imports: [PrismaLibrary],
+  providers: [PrismaLibrary],
   exports: [PrismaLibrary],
 })
 export class PrismaModule {}
