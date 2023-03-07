b25691e9428713658078aea7ebabd7e77484e68a
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-07 11:56:55 +0900

    iwip

--- src/controllers/hacker/hacker.ctl.ts
+++ src/controllers/hacker/hacker.ctl.ts
@@ -1,8 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { SetErrorResponse, SetResponse } from "dto/response.dto";
+import { HackerError } from "errors/hacker.error";
 import { BbcNewsProvider } from "libraries/providers/bbc.lib";
 import { HackersNewsProvider } from "libraries/providers/hacker.lib";
 
 @Controller("hacker")
-export class ScrapeController {
+export class HackerController {
   constructor(private readonly hacker: HackersNewsProvider) {}
+
+  @Get("/count")
+  async getHackerCount() {
+    try {
+      const count = await this.hacker.getHackerNewsCount();
+
+      return new SetResponse(200, { count });
+    } catch (error) {
+      throw new SetErrorResponse(500, error);
+    }
+  }
 }
