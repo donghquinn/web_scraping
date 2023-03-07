d0ebd0f6e6a4159fdab094676846f056dc240927
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-07 12:14:20 +0900

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import helmet from "helmet";
 import { AppModule } from "modules/app.module";
 
 export const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const port = Number(process.env.APP_PORT!);
+
+  app.enableCors();
+  app.use(helmet());
+  app.enableVersioning();
+  app.useBodyParser("json");
 
   await app.listen(port);
 
