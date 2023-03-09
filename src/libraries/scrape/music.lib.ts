921cec1000018282c677fe9899a843bf44007cdc
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-09 11:57:51 +0900

    fix: schmea music

--- src/libraries/scrape/music.lib.ts
+++ src/libraries/scrape/music.lib.ts
@@ -3,21 +3,21 @@ import * as cheerio from "cheerio";
 import { MelonError } from "errors/melon.error";
 import { MusicRank } from "types/music.type";
 
-export const scrapeMelonMusicChart = async () => {
-  try {
-  } catch (error) {
-    throw new MelonError(
-      "Melon Chart",
-      "Scrape Melon Chart Error",
-      error instanceof Error ? error : new Error(JSON.stringify(error))
-    );
-  }
-};
+// export const scrapeMelonMusicChart = async () => {
+//   try {
+//   } catch (error) {
+//     throw new MelonError(
+//       "Melon Chart",
+//       "Scrape Melon Chart Error",
+//       error instanceof Error ? error : new Error(JSON.stringify(error))
+//     );
+//   }
+// };
 
 /**
  * https://search.daum.net/search?nil_suggest=sugsch&w=tot&DA=GIQ&sq=%EC%97%B0%EB%A0%B9%EB%B3%84+%EC%9D%8C%EC%9B%90%EC%B0%A8%ED%8A%B8&o=1&sugo=11&q=%EC%97%B0%EB%A0%B9%EB%B3%84+%EC%9D%8C%EC%9B%90%EC%B0%A8%ED%8A%B8
  */
-export const scrapeMelonChartByAge = async () => {
+export const scrapeMelonChart = async () => {
   try {
     const musicArray: Array<MusicRank> = [];
 
@@ -26,17 +26,6 @@ export const scrapeMelonChartByAge = async () => {
     const { data } = await axios.get(chartByAge);
 
     const html = cheerio.load(data);
-
-    // const ageList = html(".tab_rwd.mg_cont")
-    //   .children("ul.list_tab")
-    //   .children("li")
-    //   // .children("div.tab_rwd mg_cont")
-    //   // .children("ul.list_tab")
-    //   .append("!")
-    //   .text()
-    //   .split("!");
-
-    // console.log(ageList);
 
     const list = html("span").children("a").append("!").text().split("!");
 
@@ -59,5 +48,3 @@ export const scrapeMelonChartByAge = async () => {
     );
   }
 };
-
-await scrapeMelonChartByAge();
