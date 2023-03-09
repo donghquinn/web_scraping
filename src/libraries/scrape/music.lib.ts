b8c08c15fe1a39fb15f386b1621233a3199755ba
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-09 11:56:20 +0900

    added: melon music

--- src/libraries/scrape/music.lib.ts
+++ src/libraries/scrape/music.lib.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { MelonError } from "errors/melon.error";
+import { MusicRank } from "types/music.type";
 
 export const scrapeMelonMusicChart = async () => {
   try {
@@ -18,30 +19,38 @@ export const scrapeMelonMusicChart = async () => {
  */
 export const scrapeMelonChartByAge = async () => {
   try {
-    const chartByAge =
-      "https://search.daum.net/search?nil_suggest=sugsch&w=tot&DA=GIQ&sq=%EC%97%B0%EB%A0%B9%EB%B3%84+%EC%9D%8C%EC%9B%90%EC%B0%A8%ED%8A%B8&o=1&sugo=11&q=%EC%97%B0%EB%A0%B9%EB%B3%84+%EC%9D%8C%EC%9B%90%EC%B0%A8%ED%8A%B8";
+    const musicArray: Array<MusicRank> = [];
+
+    const chartByAge = "https://www.melon.com/chart/index.htm";
+    // "https://search.daum.net/search?nil_suggest=sugsch&w=tot&DA=GIQ&sq=%EC%97%B0%EB%A0%B9%EB%B3%84+%EC%9D%8C%EC%9B%90%EC%B0%A8%ED%8A%B8&o=1&sugo=11&q=%EC%97%B0%EB%A0%B9%EB%B3%84+%EC%9D%8C%EC%9B%90%EC%B0%A8%ED%8A%B8";
     const { data } = await axios.get(chartByAge);
 
     const html = cheerio.load(data);
 
-    const ageList = html(".tab_rwd.mg_cont")
-      .children("ul.list_tab")
-      .children("li")
-      // .children("div.tab_rwd mg_cont")
-      // .children("ul.list_tab")
-      .append("!")
-      .text()
-      .split("!");
+    // const ageList = html(".tab_rwd.mg_cont")
+    //   .children("ul.list_tab")
+    //   .children("li")
+    //   // .children("div.tab_rwd mg_cont")
+    //   // .children("ul.list_tab")
+    //   .append("!")
+    //   .text()
+    //   .split("!");
 
-    console.log(ageList);
+    // console.log(ageList);
 
-    const musicList = html("span").text();
+    const list = html("span").children("a").append("!").text().split("!");
+
+    for (let i = 0; i < list.length; i += 2) {
+      musicArray.push({ title: list[i], artist: list[i + 1] });
+    }
 
     // .append("!")
     // .text()
     // .split("!");
 
-    console.log(musicList);
+    console.log(musicArray);
+
+    return musicArray;
   } catch (error) {
     throw new MelonError(
       "Melon Chart",
---
+++ src/types/music.type.ts
@@ -0,0 +1,4 @@
+export interface MusicRank {
+  title: string;
+  artist: string;
+}
