bb4e50a34498d987dd71dcd16cd14473ead31e1c
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-14 15:40:39 +0900

    wip

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from "@nestjs/common";
-import { HackerModule } from "./hacker.module";
-import { BbcModule } from "./bbc.module";
-import { PrismaModule } from "./prisma.module";
-import { MusicModule } from "./music.module";
+import { Module } from '@nestjs/common';
+import { HackerModule } from './hacker.module';
+import { BbcModule } from './bbc.module';
+import { PrismaModule } from './prisma.module';
+import { MusicModule } from './music.module';
+import { ClimateModule } from './climate.module';
 
 @Module({
-  imports: [HackerModule, BbcModule, PrismaModule, MusicModule],
+  imports: [HackerModule, BbcModule, PrismaModule, MusicModule, ClimateModule],
 })
 export class AppModule {}
