25f2269d0e7474ad58595a73736e0efa8bb73f21
Dong H. Kim <98877622+donghquinn@users.noreply.github.com>
2023-03-23 13:35:09 +0900

    Dev (#35)
    * remved un-used dependenceis
    
    * wip
    
    * added: climate module
    
    * wip
    
    * start logging
    
    * wip: music search
    
    * wip
    
    * wip
    
    * wip: naver news
    
    * iwip
    
    * wip
    
    * wip
    
    * changed typescript 5
    
    * added: music genre
    
    * added: music chart graph
    
    * wip: music chart graph
    
    * fix: moduleize
    
    * register navernews
    
    * fix: insert naver news
    
    * fix
    
    * fix: naver
    
    * wipnaver

--- src/libraries/scrape/naver.lib.ts
+++ src/libraries/scrape/naver.lib.ts
@@ -3,68 +3,9 @@ import fetch from 'node-fetch';
 import { NaverNewsResponse, NaverNewsResultReturn } from 'types/naver.type';
 import utf8 from 'utf8';
 
-// export const naverSearchLink = async () => {
-//   try {
-//     const url = 'https://openapi.naver.com/v1/datalab/search';
-
-//     const token = process.env.NAVER_TOKEN!;
-//     const client = process.env.NAVER_CLIENT!;
-
-//     const year = new Date().getFullYear();
-//     const month = new Date().getMonth();
-//     const day = new Date().getDay();
-
-//     const startDate = new Date(`${year}-${month}-${day}`);
-
-//     const endDate = subMonths(startDate, 1);
-
-//     const ageArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', ''];
-
-//     const requestOptions: Array<NaverSearchRequests> = [];
-
-//     ageArray.filter((item) => {
-//       const headers = {
-//         'X-Naver-Client-Id': client,
-//         'X-Naver-Client-Secret': token,
-//         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-//       };
-//       const options = {
-//         method: 'POST',
-//         headers,
-//         body: JSON.stringify({
-//           startDate: startDate.toString(),
-//           endDate: endDate.toString(),
-//           timeUnit: 'week',
-//           keywordGroups: { groupName: '음악차트', keywords: ['음원 차트'] },
-//           ages: item,
-//         }),
-//       };
-
-//       const age = naverAgeDicision(item);
-//       requestOptions.push({ age, options });
-//     });
-
-//     const responses = requestOptions.map(async (item) => {
-//       const result = (await (await fetch(url, item.options)).json()) as NaverDataLabResponse;
-
-//       return { age: item.age, response: result };
-//     });
-
-//     return responses;
-//   } catch (error) {
-//     throw new NaverError(
-//       'Naver Search Rank',
-//       'Naver Search Rank Error',
-//       error instanceof Error ? error : new Error(JSON.stringify(error)),
-//     );
-//   }
-// };
-
 export const naverNews = async () => {
   try {
-
     // const newsArray: Array<NaverNewsResultReturn> = [];
-
 
     const queryName = utf8.encode('IT');
 
