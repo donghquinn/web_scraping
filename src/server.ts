379336a8eed1d68c499642a9e53d4c218ab6b62b
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-29 10:53:11 +0900

    wip: logging

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import session from 'express-session';
 import helmet from 'helmet';
 import { AppModule } from 'modules/app.module';
+import { LoggerModule } from 'modules/logger.module';
 
 export const bootstrap = async () => {
   const { ScrapeObserver } = await import('libraries/manager.lib');
@@ -22,7 +24,16 @@ export const bootstrap = async () => {
   app.enableVersioning();
   app.useBodyParser('json');
   app.enableShutdownHooks();
-
+  app.useLogger(app.get(LoggerModule));
+  app.use(
+    session({
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1800000, secure: true },
+    }),
+  );
   await app.listen(port);
 
   const message = `Listening On ${port}`;
