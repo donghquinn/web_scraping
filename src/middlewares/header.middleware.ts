02f910949475aab52a3a97a16b4ebe218a865b35
Dong H. Kim <98877622+donghquinn@users.noreply.github.com>
2023-03-29 18:04:05 +0900

    Dev (#47)
    * remved un-used dependenceis
    
    * wip
    
    * added: climate module
    
    * wip
    
    * start logging
    
    * wip: music search
    
    * wip
    
    * wip
    
    * wip: naver news
    
    * iwip
    
    * wip
    
    * wip
    
    * changed typescript 5
    
    * added: music genre
    
    * added: music chart graph
    
    * wip: music chart graph
    
    * fix: moduleize
    
    * register navernews
    
    * fix: insert naver news
    
    * fix
    
    * fix: naver
    
    * wipnaver
    
    * wip
    
    * wip: dto
    
    * fix: packaage.json
    
    * updated nestjs version up
    
    * fix
    
    * logger
    
    * wip: logging
    
    * added: logger
    
    * wip: logger
    
    * wip: middleware
    
    * fix: news response
    
    * wip: controlelr
    
    * changed
    
    * added: naver provider
    
    * wip: naver
    
    * middlewares
    
    * added: middleware
    
    * fix: get naver NEws
    
    * hacker naver climate
    
    * libraires
    
    * version up
    
    * response
    
    * prisma
    
    * fix

--- src/middlewares/header.middleware.ts
+++ src/middlewares/header.middleware.ts
@@ -1,12 +1,10 @@
-import { Inject, Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Injectable()
 export class HeadersMiddleware implements NestMiddleware {
-  constructor() {}
-
   use(request: Request, response: Response, next: (error?: any) => void) {
-    Logger.debug(request.headers);
+    // Logger.debug(request.headers);
     Logger.debug(request.ip);
 
     next();
