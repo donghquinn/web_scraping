1e04151f832a1926d6b20a8ce283ad459e604138
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-29 11:28:25 +0900

    wip: middleware

---
+++ src/middlewares/header.middleware.ts
@@ -0,0 +1,13 @@
+import { Inject, Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class HeadersMiddleware implements NestMiddleware {
+  constructor() {}
+
+  use(request: Request, response: Response, next: (error?: any) => void) {
+    Logger.log(request.headers);
+
+    next();
+  }
+}
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { HackerModule } from './hacker.module';
 import { BbcModule } from './bbc.module';
 import { PrismaModule } from './prisma.module';
 import { MusicModule } from './music.module';
 import { ClimateModule } from './climate.module';
+import { HeadersMiddleware } from 'middlewares/header.middleware';
 
 @Module({
   imports: [HackerModule, BbcModule, PrismaModule, MusicModule, ClimateModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HeadersMiddleware);
+  }
+}
