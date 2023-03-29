115b5d8dc25518b3c84e461a89aa9a45f90b1d0c
Kim, Dong H <ehdgus1524@gmail.com>
2023-03-29 12:41:38 +0900

    added: naver provider

--- src/libraries/providers/climate.lib.ts
+++ src/libraries/providers/climate.lib.ts
@@ -31,48 +31,19 @@ export class ClimateProvider {
           dataTime: true,
           created: true,
         },
-        // where: { created: now.toLocaleDateString() },
-        // take: 15,
         orderBy: { dataTime: 'desc' },
       });
 
-      // if (result === null || !result) {
-      //   Logger.debug('Could Not Found Todays');
-      //   const result = await this.prisma.climate.findMany({
-      //     select: {
-      //       pm10Value: true,
-      //       no2Value: true,
-      //       o3Value: true,
-      //       coValue: true,
-      //       so2Value: true,
-      //       khaiValue: true,
-      //       o3Grade: true,
-      //       so2Grade: true,
-      //       no2Grade: true,
-      //       coGrade: true,
-      //       khaiGrade: true,
-      //       khaiStatus: true,
-      //       dataTime: true,
-      //     },
-      //     where: { created: subDays(now, 1) },
-      //     orderBy: { dataTime: 'desc' },
-      //   });
-
-      //   return result;
-      // }
-
-      // Logger.debug('Today Climate date: %o', { ...result });
       const returnArray: unknown[] = [];
 
       result.filter((item) => {
         Logger.debug('Date: %o', { created: item.created.toDateString(), now: now.toDateString() });
 
-        if (item.created.getDate() === now.getDate() - 1) {
+        if (item.created.getDate() === now.getDate()) {
           returnArray.push(item);
+        } else {
+          returnArray.push('No Result Found');
         }
-        // if (item.dataTime === now.toDateString()) {
-        //   returnArray.push(item);
-        // }
       });
       return returnArray;
     } catch (error) {
---
+++ src/libraries/providers/naver.lib.ts
@@ -0,0 +1,44 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { NaverError } from 'errors/naver.error';
+import { PrismaLibrary } from 'libraries/common/prisma.lib';
+
+@Injectable()
+export class NaverProvider {
+  constructor(private prisma: PrismaLibrary) {}
+
+  async getNaverNews() {
+    try {
+      const now = new Date();
+
+      const result = await this.prisma.naverNews.findMany({
+        select: {
+          keyWord: true,
+          title: true,
+          description: true,
+          originallink: true,
+          postedTime: true,
+          founded: true,
+        },
+        orderBy: { founded: 'desc' },
+      });
+
+      const returnArray: unknown[] = [];
+
+      result.filter((item) => {
+        Logger.debug('Date: %o', { created: item.founded.toDateString(), now: now.toDateString() });
+
+        if (item.founded.getDate() === now.getDate()) {
+          returnArray.push(item);
+        } else {
+          returnArray.push('No Result Found');
+        }
+      });
+    } catch (error) {
+      throw new NaverError(
+        'Get Today NAver News',
+        'Get Naver News Error',
+        error instanceof Error ? error : new Error(JSON.stringify(error)),
+      );
+    }
+  }
+}
