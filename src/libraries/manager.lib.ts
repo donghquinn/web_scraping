14c2aada437af505012bd314c5544ad2d1a412c3
Kim, Dong H <ehdgus1524@gmail.com>
2023-04-02 17:32:49 +0900

    wip

--- src/libraries/manager.lib.ts
+++ src/libraries/manager.lib.ts
@@ -160,6 +160,9 @@ export class ScrapeObserver {
 
   timeCheck() {
     this.runningMoment = new Date(this.now.getFullYear(), this.now.getMonth(), this.now.getDate(), 23, 59);
+
+    // Logger.debug({ now: this.now, runningMoment: this.runningMoment });
+
     if (
       this.now.getFullYear() === this.runningMoment.getFullYear() &&
       this.now.getMonth() === this.runningMoment.getMonth() &&
