c95805eed6153b68b72b83fb040d14e8be86e72d
Kim, Dong H <ehdgus1524@gmail.com>
2023-04-03 11:28:34 +0900

    wip

--- src/libraries/manager.lib.ts
+++ src/libraries/manager.lib.ts
@@ -74,21 +74,36 @@ export class ScrapeObserver {
           if (result[0].status === 'fulfilled') {
             this.scrapeResultArray.hackers = result[0].value;
           }
+          if (result[0].status === 'rejected') {
+            Logger.error('Hackers News Scrape Error: %o', { error: result[0].reason });
+          }
 
           if (result[1].status === 'fulfilled') {
             this.scrapeResultArray.bbc = result[1].value;
+          }
+          if (result[1].status === 'rejected') {
+            Logger.error('BBC News Scrape Error: %o', { error: result[1].reason });
           }
 
           if (result[2].status === 'fulfilled') {
             this.scrapeResultArray.melon = result[2].value;
           }
+          if (result[2].status === 'rejected') {
+            Logger.error('Melon Music Rank Chart Scrape Error: %o', { error: result[2].reason });
+          }
 
           if (result[3].status === 'fulfilled') {
             this.scrapeResultArray.climate = result[3].value;
           }
+          if (result[3].status === 'rejected') {
+            Logger.error('Korea Climate Scrape Error: %o', { error: result[3].reason });
+          }
 
           if (result[4].status === 'fulfilled') {
             this.scrapeResultArray.naverNews = result[4].value;
+          }
+          if (result[4].status === 'rejected') {
+            Logger.error('Naver News Scrape Error: %o', { error: result[4].reason });
           }
 
           await this.receivedDataInsert(this.scrapeResultArray);
