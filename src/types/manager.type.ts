26429eac5903c59ef716a5dd0840bcd5b2c03a73
Kim, Dong H <ehdgus1524@gmail.com>
2023-04-03 11:25:49 +0900

    fix: types

--- src/types/manager.type.ts
+++ src/types/manager.type.ts
@@ -1,11 +1,12 @@
+import { BbcNewsReturnArray } from './bbc.type';
 import { ClimateReturnData } from './climate.type';
+import { HackersNewsArrayType } from './hackers.type';
 import { MusicRank } from './music.type';
 import { NaverNewsItems } from './naver.type';
-import { NewsArrayType } from './bbc.type';
 
 export interface ScrapeResultArray {
-  bbc: NewsArrayType[];
-  hackers: NewsArrayType[];
+  bbc: BbcNewsReturnArray[];
+  hackers: HackersNewsArrayType[];
   melon: MusicRank[];
   climate: ClimateReturnData[];
   naverNews: NaverNewsItems[];
