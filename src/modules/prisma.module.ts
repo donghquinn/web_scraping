69b771e55287b99056a8cec57520294a7eaf368a
Kim, Dong H <ehdgus1524@gmail.com>
2023-04-26 09:59:28 +0900

    prisma global module

--- src/modules/prisma.module.ts
+++ src/modules/prisma.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaLibrary } from 'libraries/common/prisma.lib';
 
+@Global()
 @Module({
   providers: [PrismaLibrary],
   // 재사용 가능하도록 해당 모듈 노출
