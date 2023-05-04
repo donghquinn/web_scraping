c3602ff32a65e8fbea1012f4861cbde58f8cf19c
Kim, Dong H <ehdgus1524@gmail.com>
2023-05-04 12:01:04 +0900

    wip

---
+++ scraper/src/libraries/scrape/climate.lib.ts
@@ -0,0 +1,62 @@
+import { Logger } from '@nestjs/common';
+import { ClimateError } from 'errors/climate.error';
+import fetch from 'node-fetch';
+import { ClimateReturnData, Response } from 'types/climate.type';
+
+export const getKoreanClimate = async () => {
+  try {
+    let khaiStatus: string;
+
+    const url = process.env.KOREAN_CLIMATE!;
+    const token = process.env.KOREAN_ENCODED_TOKEN!;
+
+    const options = {
+      method: 'GET',
+    };
+
+    const response = await fetch(
+      `${url}?returnType=json&serviceKey=${token}&numOfRows=1pageNo=1&&stationName=종로구&dataTerm=DAILY&numOfRows=23`,
+      options,
+    );
+
+    const responseData = (await response.json()) as Response;
+
+    const climate = responseData.response.body.items as Array<ClimateReturnData>;
+
+    for (let i = 0; i < climate.length; i += 1) {
+      if (Number(climate[i].khaiGrade) === 1) {
+        khaiStatus = 'Good';
+        climate[i].khaiStatus = khaiStatus;
+      }
+
+      if (Number(climate[i].khaiGrade) === 2) {
+        khaiStatus = 'Normal';
+        climate[i].khaiStatus = khaiStatus;
+      }
+
+      if (Number(climate[i].khaiGrade) === 3) {
+        khaiStatus = 'Bad';
+        climate[i].khaiStatus = khaiStatus;
+      }
+
+      if (Number(climate[i].khaiGrade) >= 4) {
+        khaiStatus = 'Very Bad';
+        climate[i].khaiStatus = khaiStatus;
+      }
+    }
+
+    Logger.debug('Climate Data Inserted');
+
+    return climate;
+  } catch (error) {
+    Logger.error('Scrape Korean Climate Error: %o', {
+      error: error instanceof Error ? error : new Error(JSON.stringify(error)),
+    });
+
+    throw new ClimateError(
+      'Korean Climate',
+      'Get Korean Climate Error',
+      error instanceof Error ? error : new Error(JSON.stringify(error)),
+    );
+  }
+};
---
+++ scraper/src/types/bbc.type.ts
@@ -0,0 +1,10 @@
+export interface BbcNewsReturnArray {
+  rank: string;
+  post: string;
+  link: string;
+}
+
+export interface NewsResponse {
+  post: string;
+  link: string;
+}
