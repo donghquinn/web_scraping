80c51a83e56df3d1e38f1b619af883752616448c
Dong H. Kim <98877622+donghquinn@users.noreply.github.com>
2023-05-06 22:34:20 +0900

    Dev (#114)
    * remved un-used dependenceis
    
    * wip
    
    * added: climate module
    
    * wip
    
    * start logging
    
    * wip: music search
    
    * wip
    
    * wip
    
    * wip: naver news
    
    * iwip
    
    * wip
    
    * wip
    
    * changed typescript 5
    
    * added: music genre
    
    * added: music chart graph
    
    * wip: music chart graph
    
    * fix: moduleize
    
    * register navernews
    
    * fix: insert naver news
    
    * fix
    
    * fix: naver
    
    * wipnaver
    
    * wip
    
    * wip: dto
    
    * fix: packaage.json
    
    * updated nestjs version up
    
    * fix
    
    * logger
    
    * wip: logging
    
    * added: logger
    
    * wip: logger
    
    * wip: middleware
    
    * fix: news response
    
    * wip: controlelr
    
    * changed
    
    * added: naver provider
    
    * wip: naver
    
    * middlewares
    
    * added: middleware
    
    * fix: get naver NEws
    
    * hacker naver climate
    
    * libraires
    
    * version up
    
    * response
    
    * prisma
    
    * fix
    
    * wip: schema
    
    * changed: scrape
    
    * added: metro
    
    * scrape error logging
    
    * logging for providers
    
    * wip error loggnig
    
    * time check
    
    * added: shutdown
    
    * wip
    
    * fix: logging level
    
    * wip: logging
    
    * changed start restriction statement
    
    * fix: timecheck month day year removed temporarily
    
    * fix: assign new date info
    
    * wip
    
    * fix: scrape promise
    
    * fix: type seperate
    
    * fix: types
    
    * wip
    
    * wip
    
    * wip:wq
    
    * wip : promise allsettled
    
    * rm unsed imported libraries
    
    * fix: bbc news
    
    * fix
    
    * fix: prisma schema -> if null
    
    * changed postgres timezone
    
    * fix
    
    * fix
    
    * fix
    
    * added: middleware
    
    * fix
    
    * rm debugging
    
    * fix: date
    
    * bring query changed
    
    * wip
    
    * fix: date
    
    * fix: climate data
    
    * fix
    
    * wip
    
    * fix yarn version
    
    * fix
    
    * fix readme
    
    * fix: eample env
    
    * wip
    
    * wip: Dockerfile
    
    * fix: dependencies version up
    
    * fix: assets
    
    * fix:
    
    * fix: scheduling
    
    * fix
    
    * fix logging
    
    * fix: logging
    
    * fix :wq
    
    * fix: kts-;path version up
    
    * wip
    
    * fix
    
    * wip
    
    * dependencies version up
    
    * wip
    
    * fix
    
    * dfix
    
    * fix schemduler
    
    * ifx
    
    * debugging
    
    * fix
    
    * fix: dependencies
    
    * wip
    
    * fix: Modulized
    
    * prisma global module
    
    * fix time
    
    * fix
    
    * Merge branch 'master' of github.com:donghquinn/web_scraping into dev
    
    * fix: removed session
    
    * wip
    
    * fix
    
    * wip
    
    * rm kin
    
    * wip
    
    * wip
    
    * move
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * iwip
    
    * wip
    
    * fix
    
    * wip
    
    * fix: docker-compose
    
    * wip
    
    * fix: script
    
    * p# Please enter the commit message for your changes. Lines starting
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * changed logs
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- scraper/src/libraries/manager.lib.ts
+++ scraper/src/libraries/manager.lib.ts
@@ -1,3 +1,4 @@
+import { ManagerError } from 'errors/manager.error';
 import schedule, { RecurrenceRule } from 'node-schedule';
 import { BbcNewsReturnArray } from 'types/bbc.type';
 import { ClimateReturnData } from 'types/climate.type';
@@ -52,57 +53,15 @@ export class ScrapeObserver {
   }
 
   public start() {
-    schedule.scheduleJob('0 10 23 * * *', async () => {
+    schedule.scheduleJob('0 59 23 * * *', async () => {
       try {
         Logger.info('Scrape Start');
 
-        const result = await Promise.allSettled([
-          scrapeHackerNews(),
-          scrapeBbcTechNews(),
-          scrapeMelonChart(),
-          getKoreanClimate(),
-          naverNews(),
-        ]);
-
-        if (result[0].status === 'fulfilled') {
-          Logger.debug('Founded Hacker News: ', result[0].value);
-          this.hacker = result[0].value;
-          Logger.debug('Hacker News Array: ', this.hacker);
-        } else if (result[0].status === 'rejected') {
-          Logger.error('Hackers News Scrape Error: %o', { error: result[0].reason });
-        }
-
-        if (result[1].status === 'fulfilled') {
-          Logger.debug('Founded BBC News: ', result[1].value);
-          this.bbc = result[1].value;
-          Logger.debug('BBC News Array: ', this.bbc);
-        } else if (result[1].status === 'rejected') {
-          Logger.error('BBC News Scrape Error: %o', { error: result[1].reason });
-        }
-
-        if (result[2].status === 'fulfilled') {
-          Logger.debug('Found Melon Music: ', result[2].value);
-          this.melon = result[2].value;
-          Logger.debug('Melon Music Array: ', this.melon);
-        } else if (result[2].status === 'rejected') {
-          Logger.error('Melon Music Rank Chart Scrape Error: %o', { error: result[2].reason });
-        }
-
-        if (result[3].status === 'fulfilled') {
-          Logger.debug('Founded Climate Data', result[3].value);
-          this.climate = result[3].value;
-          Logger.debug('Climate Array: ', this.climate);
-        } else if (result[3].status === 'rejected') {
-          Logger.error('Korea Climate Scrape Error: %o', { error: result[3].reason });
-        }
-
-        if (result[4].status === 'fulfilled') {
-          Logger.debug('Founded Naver News: ', this.naver);
-          this.naver = result[4].value;
-          Logger.debug('Naver News Array: ', this.naver);
-        } else if (result[4].status === 'rejected') {
-          Logger.error('Naver News Scrape Error: %o', { error: result[4].reason });
-        }
+        await scrapeHackerNews();
+        await scrapeBbcTechNews();
+        await scrapeMelonChart();
+        await getKoreanClimate();
+        await naverNews();
 
         await this.receivedDataInsert(this.bbc, this.naver, this.hacker, this.melon, this.climate);
       } catch (error) {
@@ -124,36 +83,28 @@ export class ScrapeObserver {
     melon: Array<MusicRank>,
     climate: Array<ClimateReturnData>,
   ) {
-    const result = await Promise.allSettled([
-      this.insert.insertBbcData(bbc),
-      this.insert.insertClimateData(climate),
-      this.insert.insertHackerNewsData(hacker),
-      this.insert.insertMelonData(melon),
-      this.insert.insertNaverNews(naver),
-    ]);
+    try {
+      await this.insert.insertBbcData(bbc);
+  
+      await  this.insert.insertClimateData(climate);
+  
+      await  this.insert.insertHackerNewsData(hacker);
+  
+      await  this.insert.insertMelonData(melon);
+  
+      await this.insert.insertNaverNews(naver);
+  
+      return true;
+    } catch (error) {
+      Logger.error("Insert Scraped Data Error: %o", {
+        error: error instanceof Error ? error : new Error(JSON.stringify(error))
+      });
 
-    const runResult = result.map<string>((item) => {
-      if (item.status === 'rejected') {
-        Logger.error('Insert Data Failed: %o', { reason: item.reason });
-
-        return item.reason;
-      }
-
-      if (item.status === 'fulfilled') {
-        Logger.info('Data Insert Finished');
-
-        this.hacker.length = 0;
-        this.bbc.length = 0;
-        this.naver.length = 0;
-        this.climate.length = 0;
-        this.melon.length = 0;
-
-        return 'success';
-      }
-
-      throw new Error("Run Scrape Failed");
-    });
-
-    return runResult;
+      throw new ManagerError(
+        "Scrape Manager", 
+        "Data Insert Error", 
+        error instanceof Error ? error : new Error(JSON.stringify(error)),
+      );
+    }
   }
 }
